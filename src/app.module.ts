382d8c99691164386c6e42245c7c9d3af42a0d2b
Himanshu Gunwant <himanshu.gunwant@acefone.com>
2025-05-31 08:39:06 +0000

    feat: add health route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { JobModule } from './job/job.module';
@@ -9,6 +8,6 @@ import { JobApplicationModule } from './job-application/job-application.module';
 @Module({
   imports: [AuthModule, UserModule, JobModule, JobApplicationModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
