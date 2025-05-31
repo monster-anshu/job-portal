a609ab0c19fedbe2d97d54e9cb9a5d7df76d552d
Himanshu Gunwant <himanshu.gunwant@acefone.com>
2025-05-31 06:44:36 +0000

    init project

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { patchNestJsSwagger, ZodValidationPipe } from 'nestjs-zod';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api/v1');
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Job Portal Apis')
+    .setVersion('1.0');
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config.build());
+
+  SwaggerModule.setup('docs', app, documentFactory, {
+    useGlobalPrefix: true,
+    jsonDocumentUrl: 'swagger.json',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+patchNestJsSwagger();
+bootstrap();
